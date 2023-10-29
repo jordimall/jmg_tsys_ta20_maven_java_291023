d5900cd149850b99de38afd5a7187f7e45118c8f
jordimall <jordi.mallafre@hotmail.com>
2023-10-29 19:59:24 +0100

    ejercicio 2 finalizado

---
+++ TA20_MAVEN_ejercicio02/src/main/java/TA20_MAVEN_ejercicio02/TA20_MAVEN_ejercicio02/App.java
@@ -0,0 +1,14 @@
+package TA20_MAVEN_ejercicio02.TA20_MAVEN_ejercicio02;
+
+import view.ViewEjercicio02;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		ViewEjercicio02 frame = new ViewEjercicio02();
+		frame.setVisible(true);
+	}
+}
---
+++ TA20_MAVEN_ejercicio02/src/test/java/TA20_MAVEN_ejercicio02/TA20_MAVEN_ejercicio02/AppTest.java
@@ -0,0 +1,20 @@
+package TA20_MAVEN_ejercicio02.TA20_MAVEN_ejercicio02;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
