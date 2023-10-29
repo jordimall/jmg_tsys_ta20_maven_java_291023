a46267b9df1078aa94306195e3e403d9af80d9a5
jordimall <jordi.mallafre@hotmail.com>
2023-10-29 22:46:15 +0100

    Finalización ejercicio 6

---
+++ TA20_MAVEN_ejercicio06/src/main/java/TA20_MAVEN_ejercicio06/TA20_MAVEN_ejercicio06/App.java
@@ -0,0 +1,10 @@
+package TA20_MAVEN_ejercicio06.TA20_MAVEN_ejercicio06;
+
+import view.ViewEjercicio06;
+
+public class App {
+	public static void main(String[] args) {
+		ViewEjercicio06 frame = new ViewEjercicio06();
+		frame.setVisible(true);
+	}
+}
