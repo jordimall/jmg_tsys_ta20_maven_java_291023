4d7f2b742a7583defaae7fddbebe9170533e051f
jordimall <jordi.mallafre@hotmail.com>
2023-10-29 20:52:31 +0100

    Finalizacion ejercicio 4

---
+++ TA20_MAVEN_ejercicio04/src/main/java/TA20_MAVEN_ejercicio04/TA20_MAVEN_ejercicio04/App.java
@@ -0,0 +1,10 @@
+package TA20_MAVEN_ejercicio04.TA20_MAVEN_ejercicio04;
+
+import view.ViewEjercicio04;
+
+public class App {
+	public static void main(String[] args) {
+		ViewEjercicio04 frame = new ViewEjercicio04();
+		frame.setVisible(true);
+	}
+}
