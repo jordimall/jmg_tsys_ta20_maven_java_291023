93f3b45e23737e44502331b84fc8d429ddfbe2da
jordimall <jordi.mallafre@hotmail.com>
2023-10-29 21:07:18 +0100

    ejercicio 5 finalizado

---
+++ TA20_MAVEN_ejercicio05/src/main/java/TA20_MAVEN_ejercicio05/TA20_MAVEN_ejercicio05/App.java
@@ -0,0 +1,10 @@
+package TA20_MAVEN_ejercicio05.TA20_MAVEN_ejercicio05;
+
+import view.ViewEjercicio05;
+
+public class App {
+	public static void main(String[] args) {
+		ViewEjercicio05 frame = new ViewEjercicio05();
+		frame.setVisible(true);
+	}
+}
